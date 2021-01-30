041cdeb74fe5066889da35110d6ac11dbf879eb9
Andrej Antunovikj <tunas@cryptolab.net>
2021-01-31 03:01:52 +0400

    Fixed typo, tried to fix matching problem

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -46,7 +46,7 @@
                     <form method="post" id='edit_userinfo' name="edit_userinfo" action="{{ url('/app/profile/update') }}">
                         {{ csrf_field() }}
                         <div class="mt-4">
-                            <x-jet-label for="gender" value="{{ __('Preferred gender(s) (or, which gender you are)') }}" />
+                            <x-jet-label for="gender" value="{{ __('Your gender (or, which gender you are)') }}" />
                             <div class="alert alert-danger" id='nogender-own' style='display:none;'><i>Please choose your gender.</i></div>
                             <div class="col-md-6 preferredGenders">
                                 <input type="radio" name="gender" value="m" @if ($gender == 'm') checked @endif required/> Male
